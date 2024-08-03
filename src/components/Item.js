79bf18adf54ef0b812f64cb8ff9b574879971323
Joshua Nyambane <nyamsjosh@gmail.com>
2024-08-03 11:38:13 +0300

    Initial commit

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [item, setItem] = useState(0);
+
+  function  handleClick(){
+    setItem("in-cart")
+  }
+
   return (
-    <li className="">
+    <li className={item}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>Add to Cart</button>
     </li>
+    //{item ? "Remove from Cart" : "Add to Cart"}
   );
 }
 
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,24 +1,36 @@
-import React from "react";
-import Item from "./Item";
+import React, { useState } from 'react'
+import Item from './Item'
 
 function ShoppingList({ items }) {
+  const [selectCategory, setSelectCategory] = useState('All')
+  const valueUpdate = (e) => {
+    setSelectCategory(e.target.value)
+  }
+
+  const itemDisplay = items.filter((item) => {
+    if (selectCategory === 'All') {
+      return true
+    } else {
+      return item.category === selectCategory
+    }
+  })
   return (
-    <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
+    <div className='ShoppingList'>
+      <div className='Filter'>
+        <select name='filter' onChange={valueUpdate}>
+          <option value='All'>Filter by category</option>
+          <option value='Produce'>Produce</option>
+          <option value='Dairy'>Dairy</option>
+          <option value='Dessert'>Dessert</option>
         </select>
       </div>
-      <ul className="Items">
-        {items.map((item) => (
+      <ul className='Items'>
+        {itemDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
     </div>
-  );
+  )
 }
 
-export default ShoppingList;
+export default ShoppingList
